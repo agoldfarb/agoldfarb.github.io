747af60c0c171c9baacf400943aa4802815e4a1c
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-11-17 15:26:31 -0600

    again

--- home.js
+++ home.js
@@ -1,8 +1,14 @@
 $(document).ready( function() {
+  //  listen for clicks on .navbar_link
   $('.navigation_link').click(function() {
+    //  grab ID value of the clicked element
     var linkId = $(this).attr('id');
+    // select corresponding box
     var box = $('.detailBox#' + linkId);
+
+    //  hide all other boxes...
     $('.detailBox').not(box).fadeOut("fast",function(){
+      //...and then show this one after
       box.fadeIn('fast');
     });
   });
